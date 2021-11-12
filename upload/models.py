206ad7d4379b0a1d1f224baa90918af8818172c6
Jan Poonthong <janpoonthong628@gmail.com>
2021-11-12 15:43:56 +0700

    first commit

---
+++ upload/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Upload(models.Model):
+    password = models.CharField(max_length=255, blank=True, null=True)
+    max_downloads = models.IntegerField()
+    expire_date = models.DateTimeField()
+    # TODO: Add more fields if needed
+
+    def get_absolute_url(self):
+        return reverse("download", args=(self.id,))
