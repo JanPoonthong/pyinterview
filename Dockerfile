ae3774a9f250b0c7fbf71911a2d84b43b73692bb
Jan Poonthong <janpoonthong628@gmail.com>
2021-11-16 15:39:02 +0700

    Config Dockerfile to work on docker image

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,8 @@
+# syntax=docker/dockerfile:1
 FROM python:3
-MAINTAINER Jan Poonthong "janpoonthong628@gmail.com"
-
 ENV PYTHONUNBUFFERED=1
-ENV DEBUG 0
-
+ENV DEBUG=False
 WORKDIR /code
 COPY requirements.txt /code/
-RUN python -m pip install --upgrade pip && pip install -r requirements.txt
+RUN pip install -r requirements.txt
 COPY . /code/
-
-# collect static files
-RUN python manage.py collectstatic --noinput
-
-RUN python manage.py makemigrations
-RUN python manage.py migrate --run-syncdb
--- send/settings.py
+++ send/settings.py
@@ -1,22 +1,24 @@
-import os
+from pathlib import Path
 import environ
 
 env = environ.Env()
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "jkkv-2fh%*z^e51r=x7ttjr$w1iec3q-vp2)glmmq9^by(o@s7"
+SECRET_KEY = (
+    "django-insecure-sglr5ubfbav1jj549_0^oc2631to8^y)2bojef1247l5*2=oue"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=True)
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["*"])
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=['localhost', '127.0.0.1'])
 
 
 # Application definition
@@ -64,17 +66,18 @@ WSGI_APPLICATION = "send.wsgi.application"
 
 
 # Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": env.db(
-        default="sqlite:///" + os.path.join(BASE_DIR, "db.sqlite3")
-    ),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 
 # Password validation
-# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -93,11 +96,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/3.0/topics/i18n/
+# https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "Asia/Bangkok"
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -107,10 +110,12 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = env("STATIC_ROOT", default=os.path.join(BASE_DIR, "staticfiles"))
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
